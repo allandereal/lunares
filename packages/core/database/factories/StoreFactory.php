5a7af7277270f25b5926a1146aaed185cbef99ae
Allan Dereal <ahullan@gmail.com>
2024-01-28 17:12:52 +0300

    add store factory for seeding

---
+++ packages/core/database/factories/StoreFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lunar\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Lunar\FieldTypes\Text;
+use Lunar\Models\Brand;
+use Lunar\Models\Product;
+use Lunar\Models\ProductType;
+
+class StoreFactory extends Factory
+{
+    protected $model = Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => new Text($this->faker->name),
+            'email' => $this->faker->email,
+            'phone' => $this->faker->e164PhoneNumber,
+            'address' => $this->faker->sentence,
+            'logo' => $this->faker->url,
+            'status' => array_rand([0,1]),
+            'product_type_id' => ProductType::factory(),
+            'content' => collect([
+                'name' => new Text($this->faker->name),
+                'description' => $this->faker->sentence,
+                'address' => $this->faker->sentence,
+            ]),
+        ];
+    }
+}
