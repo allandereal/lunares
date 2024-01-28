b1a7f5f8cf0f87ef3f6a7754f61fa3bd00f014df
Allan Dereal <ahullan@gmail.com>
2024-01-28 18:53:31 +0300

    fix user -store foreign key

---
+++ packages/core/database/migrations/2021_07_29_100014_add_store_id_foreign_key_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Lunar\Base\Migration;
+
+class AddStoreIdForeignKeyToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->prefix.'users', function (Blueprint $table) {
+            $table->foreign('store_id')
+                ->references('id')
+                ->on($this->prefix.'store')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->prefix.'users', function (Blueprint $table) {
+            $table->dropForeign('store_id');
+        });
+    }
+}
